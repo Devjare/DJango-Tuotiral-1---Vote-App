6b5e0f19b290fc041fd5a62740447f5a45fad07f
Andres Reyna <andresreyna15@gmail.com>
2023-12-13 16:30:32 -0600

    Finished part 4. Part 5: Tests

--- polls/urls.py
+++ polls/urls.py
@@ -5,10 +5,18 @@ from . import views
 # in case another app in the project has shared view names
 # https://docs.djangoproject.com/en/4.2/intro/tutorial03/#namespacing-url-names.
 app_name = "polls"
-
+# Using Django generic views
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("details/<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/results", views.results, name="results"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("details/<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
+
+# "hard-coded way"
+# urlpatterns = [
+#     path("", views.index, name="index"),
+#     path("details/<int:question_id>/", views.detail, name="detail"),
+#     path("<int:question_id>/results", views.results, name="results"),
+#     path("<int:question_id>/vote/", views.vote, name="vote"),
+# ]
