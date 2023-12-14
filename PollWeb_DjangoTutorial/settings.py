582be9049d0ddbe54ad5886fdeb982fa9af625a2
Andres Reyna <andresreyna15@gmail.com>
2023-12-13 23:15:18 -0600

    Finished tutorial app

--- PollWeb_DjangoTutorial/settings.py
+++ PollWeb_DjangoTutorial/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "polls.apps.PollsConfig",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -55,7 +57,7 @@ ROOT_URLCONF = "PollWeb_DjangoTutorial.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -100,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# IPS FOR DebugToolbar Third-party package.
+INTERNAL_IPS = ["127.0.0.1"]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
